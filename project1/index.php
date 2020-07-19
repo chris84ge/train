3dcc873e786de682dacfdd2e4349c558359d38c5
Chris <40881128+chris84ge@users.noreply.github.com>
2020-07-19 12:01:45 +0200

    added functionality
    session not working :/

--- project1/index.php
+++ project1/index.php
@@ -1,18 +1,51 @@
 <?php
   include 'head.php';
+  if(filter_has_var(INPUT_POST, 'submit')){
+    $name = $_POST['name'];
+    $email = $_POST['name'];
+    if(strlen($name)>10){
+      $msg = "$name is a long name!";
+    } else {
+      $msg = "$name is a regular name";
+    }
+  }
+
+  if(isset($_POST['more'])){
+    session_start();
+    $_SESSION['name'] = $name;
+    $_SESSION['email'] = $email;
+    header('Location: page2.php');
+  }
+
+  if(filter_has_var(INPUT_POST, 'reset')){
+    $name = "";
+    $email = "";
+  }
+
 ?>
 
   <main class="container">
     <section>
-      <p>This form does nothing just yet</p>
+      <p>This form get and display your input</p>
     </section>
-    <form>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
       <label for="name">Name</label>
-      <input type="text" name="name" id="name"><br>
+      <input type="text" name="name" id="name" ><br>
       <label for="email">Email</label>
-      <input type="text" name="email" id="email"><br>
+      <input type="text" name="email" id="email" ><br>
       <input type="submit" name="submit">
-    </form>
+      <input type="submit" name="reset" value="Reset">
+      <?php if(filter_has_var(INPUT_POST, 'submit')): ?>
+        <input type="submit" name="more" value="Click here for more">
+      <?php endif; ?>
+    </form><br>
+    <?php if(filter_has_var(INPUT_POST, 'submit')): ?>
+    <section>
+      <p><?php echo "Il tuo nome e' $name";?></p>
+      <p><?php echo "La tua email e' $email";?></p>
+      <p><?php echo $msg;?></p>
+    </section>
+  <?php endif; ?>
   </main>
 </body>
 
--- project1/page2.php
+++ project1/page2.php
@@ -1,8 +1,12 @@
 <?php
   include 'head.php';
+  session_start();
+  $name = isset($_SESSION['name']) ? $_SESSION["name"] : "Guest";
+  $email = $_SESSION['email'];
 ?>
 
-  <p>This is page 2, nothing to see for now</p>
+  <p><?php echo "Hello $name";?></p>
+  <p><?php echo "Your mail is $email, right?";?></p>
 </body>
 
 <?php
